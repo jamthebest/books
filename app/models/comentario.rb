66839b6817e989da1a85ea5ae8b9d180e2e893e5
Javier Midence <javier.midence@unitec.edu>
2012-06-15 09:45:34 -0600

    campo nuevo en vista index usuario

--- app/models/comentario.rb
+++ app/models/comentario.rb
@@ -1,16 +1,13 @@
 class Comentario < ActiveRecord::Base
-#<<<<<<< HEAD
   attr_accessible :contenido, :perfil, :user
 
   validates_presence_of :contenido, :perfil, :user
 
   belongs_to :user
 	
-#=======
   attr_accessible :contenido, :perfil, :user_id
 
   validates_presence_of :contenido
 
   belongs_to :user
-#>>>>>>> 2c05d65856763c0beb1797f8206b8c873954ccdb
 end
