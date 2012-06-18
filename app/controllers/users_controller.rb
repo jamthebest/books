302da0698ba986e943b8f69430329584a7cf76f9
Javier Midence <javier.midence@unitec.edu>
2012-06-18 10:19:55 -0600

    Años en new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,6 @@ def index
     respond_to do |format|
      if @user.save
         session[:user_id] = @user.id
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.html { redirect_to @user, notice: 'Usuario Creado!' }
         format.json { render json: @user, status: :created, location: @user }
      else
